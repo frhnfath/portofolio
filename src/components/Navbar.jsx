42f927e8ef41a385220450180653cd8826d9e2d1
frhnfath <frhnfath_farhan@apps.ipb.ac.id>
2024-07-01 13:16:22 +0700

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,19 @@ import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import { CONTACT } from "../constants";
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between py-6">
+    <nav className="fixed top-0 z-10">
+      <div className="flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo" srcset="" />
+            {/* <img className="mx-2 w-10" src={logo} alt="logo" srcset="" /> */}
         </div>
         <div className="mb-8 flex items-center justify-center gap-4 text-2xl">
-            <FaLinkedin />
-            <FaGithub />
-            <FaSquareXTwitter />
-            <FaInstagram />
+            <a href={CONTACT.linkedin}><FaLinkedin /></a>
+            <a href={CONTACT.github}><FaGithub /></a>
         </div>
+      </div>
     </nav>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -54,5 +54,6 @@ export const CONTACT = {
   address: "767 Fifth Avenue, New York, NY 10153 ",
   phoneNo: "+12 4555 666 00 ",
   email: "farhan.fathurrahman2001@gmail.com",
-  github: "https://github.com/frhnfath"
+  github: "https://github.com/frhnfath",
+  linkedin: "https://www.linkedin.com/in/frhnfath"
 };
